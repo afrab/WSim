2b9d25bf00de520cb2eef857b300a3fbe947c6fa
afraboul <afraboul@c0ef0dea-404d-0410-ad65-84cceb5f200a>
2009-03-21 06:41:38 +0000

    - HEX file parser (16 fields only) for flash preload
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/wsim@104 c0ef0dea-404d-0410-ad65-84cceb5f200a

--- arch/common/ihex.c
+++ arch/common/ihex.c
@@ -9,24 +9,130 @@
 #include <stdio.h>
 #include "hardware.h"
 
-#define MAXLINE 50
+/* ************************************** */
+/* ************************************** */
+/* ************************************** */
 
-#define H1L 1
-#define H2L 2
+#define H1L 2
+#define H2L 6
+#define MAXLINE 100
 
 /* ************************************** */
 /* ************************************** */
 /* ************************************** */
 
-/*
-wsim-wsn430  --trace=wsim.trc --serial1_io=udp:localhost:6000:localhost:7000 \
-  --mode=time --modearg=0s --precharge=wsim.trc --logfile=stdout --verbose=3 \
-  wsn430-serial.elf --help
-*/
+#define DATA_RECORD   00  /* Data                  */
+#define EOF_RECORD    01  /* End of File           */
+#define ESA_RECORD    02  /* Extended Segment Addr */
+#define SSA_RECORD    03  /* Segment Start Addr    */
+#define ELA_RECORD    04  /* Extended Linear Addr  */
+#define SLA_RECORD    05  /* Start Linear Addr     */
 
-void ihex_read_line(char line[MAXLINE])
+
+int htoi(char c)
 {
-  VERBOSE(H2L,"wsim:ihex:line: %s",line);
+  if ((c >= '0') && (c <= '9'))
+    return c - '0';
+  if ((c >= 'a') && (c <= 'f'))
+    return c -'a' + 10;
+  if ((c >= 'A') && (c <= 'F'))
+    return c -'A' + 10;
+  return 0;
+}
+
+uint8_t hex2int(char c1, char c2)
+{
+#define HEX_BIGENDIAN
+#if defined(HEX_BIGENDIAN)
+  return (htoi(c1) << 4) + htoi(c2); // bigendian
+#else
+  return (htoi(c2) << 4) + htoi(c1); // littleendian
+#endif
+}
+
+/* ************************************** */
+/* ************************************** */
+/* ************************************** */
+
+int ihex_read_line(char line[MAXLINE])
+{
+  char   *hexline;
+  uint8_t i,nbytes,checksum;
+  uint8_t bytes[MAXLINE];
+
+  VERBOSE(H2L,"wsim:ihex:line: %s ",line);
+
+  if (line[0] != ':')
+    {
+      VERBOSE(H2L,"does not begin with :\n");
+      return -1;
+    }
+  
+  hexline = &line[1];
+  if ((strlen(hexline) & 0x01) == 0x1)
+    {
+      VERBOSE(H2L,"size is not a multiple of 2\n");
+      return -1;
+    }
+
+  for(i=0, nbytes=0, checksum=0; i<(strlen(hexline)/2); i++)
+    {
+      bytes[i]  = hex2int(hexline[i*2], hexline[i*2+1]);
+      checksum += bytes[i];
+      nbytes ++;
+    }
+
+  if (checksum != 0)
+    {
+      VERBOSE(H2L,"bad checksum %02x %02x\n",checksum,bytes[nbytes - 1]);
+      return -1;
+    }
+
+#if defined(DEBUG_ME_HARDER)
+  for(i=0; i<nbytes; i++)
+    {
+      VERBOSE(H2L,"%02x",bytes[i]);
+    }
+  VERBOSE(H2L,"\n");
+#endif
+
+  int      count = (bytes[0]);
+  uint16_t addr  = (bytes[1] << 8) | bytes[2];
+  uint8_t  type  = (bytes[3]); 
+  VERBOSE(H2L,":: %d bytes, addr 0x%04x, type %02x=",count,addr,type);
+
+  switch (type)
+    {
+    case DATA_RECORD   :  /* Data                  */
+      VERBOSE(H2L,"Data :: ");
+      for(i=0; i<count; i++)
+	{
+	  VERBOSE(H2L,"%02x ",bytes[4+i]);
+	  mcu_jtag_write_byte(addr+i,bytes[4+i]);
+	}
+      VERBOSE(H2L,"\n");
+      return 0;
+    case EOF_RECORD    :  /* End of File           */
+      VERBOSE(H2L,"End of File\n");
+      return 0;
+    case ESA_RECORD    :  /* Extended Segment Addr */
+      VERBOSE(H2L,"Extended Segment Addr\n");
+      return -1;
+    case SSA_RECORD    :  /* Segment Start Addr    */
+      VERBOSE(H2L,"Segment Start Addr\n");
+      return -1;
+    case ELA_RECORD    :  /* Extended Linear Addr  */
+      VERBOSE(H2L,"Extended Linear Addr\n");
+      return -1;
+    case SLA_RECORD    :  /* Start Linear Addr     */
+      VERBOSE(H2L,"Start Linear Addr\n");
+      return -1;
+    default:
+      VERBOSE(H2L,"Unknown line type\n");
+      return -1;
+    }
+
+  return -1;
 }
 
 /* ************************************** */
@@ -39,8 +145,6 @@ int mcu_hexfile_load(char *filename)
   char buff[MAXLINE];
 
   VERBOSE(H1L,"wsim:precharge: loading hexfile %s\n",filename);
-  //	  mcu_jtag_write_section(elf->file_raw + s->sh_offset, s->sh_addr, s->sh_size);
-  //	  mcu_jtag_write_zero(s->sh_addr,s->sh_size);
 
   if ((f = fopen(filename,"rb")) == NULL)
     {
@@ -50,7 +154,14 @@ int mcu_hexfile_load(char *filename)
 
   while(fgets(buff,MAXLINE,f) != NULL)
     {
-      ihex_read_line(buff);
+      if (buff[strlen(buff) -1] == '\n' || buff[strlen(buff) -1] == '\r')
+	buff[strlen(buff) -1] = '\0';
+
+      if (ihex_read_line(buff) == -1)
+	{
+	  fclose(f);
+	  return -1;
+	}
     }
 
   fclose(f);

85b5e2975433ef88af826532ee8f410125141612
llemaitre <llemaitre@c0ef0dea-404d-0410-ad65-84cceb5f200a>
2010-08-27 08:22:49 +0000

    Upgrade of liblogpkt: add a buffer structure to handle 2 different events during at a sync point (end of a packet, reception of the first byte of the next packet for example)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/wsim@463 c0ef0dea-404d-0410-ad65-84cceb5f200a

--- liblogpkt/logpkt.c
+++ liblogpkt/logpkt.c
@@ -58,12 +58,21 @@ struct _logpkt_state_t {
   uint64_t rx_end_time;
   uint64_t tx_start_time;
   uint64_t tx_end_time;
+};
+
+struct _logpkt_state_no_bk_t {
+  char     interface_name[MAXINTERFACENAME];
   uint32_t rx_pkt_count;
   uint32_t tx_pkt_count;
 };
 
-static struct _logpkt_state_t logpkt_tab      [MAX_INTERFACES]; /* current logpkt table             */
-static struct _logpkt_state_t logpkt_tab_saved[MAX_INTERFACES]; /* saved logpkt table for backtrack */
+static struct _logpkt_state_t logpkt_tab          [MAX_INTERFACES]; /* current logpkt table                      */
+static struct _logpkt_state_t logpkt_tab_saved    [MAX_INTERFACES]; /* saved logpkt table for backtrack          */
+static struct _logpkt_state_t logpkt_tab_buf      [MAX_INTERFACES]; /* buffered logpkt table                     */
+static struct _logpkt_state_t logpkt_tab_buf_saved[MAX_INTERFACES]; /* saved buffered logpkt table for backtrack */
+
+/* not buffered, not backtracked */
+static struct _logpkt_state_no_bk_t logpkt_no_bk_tab[MAX_INTERFACES];
 
 static char  logpkt_filename[MAXFILENAME];
 static FILE* logpkt_logfile;
@@ -205,23 +214,33 @@ void logpkt_init_interface_op(int interface_id, const char* interface_name)
     }
 
   /* init variables */
-  strcpy(logpkt_tab[interface_id].interface_name, interface_name);
-  logpkt_tab[interface_id].rx_pkt_offset    = 0;
-  logpkt_tab[interface_id].tx_pkt_offset    = 0;
-  logpkt_tab[interface_id].rx_pkt_completed = 0;
-  logpkt_tab[interface_id].tx_pkt_completed = 0;
-  logpkt_tab[interface_id].rx_pkt_count     = 0;
-  logpkt_tab[interface_id].tx_pkt_count     = 0;
+  logpkt_tab[interface_id].rx_pkt_offset     = 0;
+  logpkt_tab[interface_id].tx_pkt_offset     = 0;
+  logpkt_tab[interface_id].rx_pkt_completed  = 0;
+  logpkt_tab[interface_id].tx_pkt_completed  = 0;
   strcpy(logpkt_tab[interface_id].tx_error_log, "");
 
-  strcpy(logpkt_tab_saved[interface_id].interface_name, interface_name);
   logpkt_tab_saved[interface_id].rx_pkt_offset    = 0;
   logpkt_tab_saved[interface_id].tx_pkt_offset    = 0;
   logpkt_tab_saved[interface_id].rx_pkt_completed = 0;
   logpkt_tab_saved[interface_id].tx_pkt_completed = 0;
-  logpkt_tab_saved[interface_id].rx_pkt_count     = 0;
-  logpkt_tab_saved[interface_id].tx_pkt_count     = 0;
   strcpy(logpkt_tab_saved[interface_id].tx_error_log, "");
+
+  logpkt_tab_buf[interface_id].rx_pkt_offset    = 0;
+  logpkt_tab_buf[interface_id].tx_pkt_offset    = 0;
+  logpkt_tab_buf[interface_id].rx_pkt_completed = 0;
+  logpkt_tab_buf[interface_id].tx_pkt_completed = 0;
+  strcpy(logpkt_tab_buf[interface_id].tx_error_log, "");
+
+  logpkt_tab_buf_saved[interface_id].rx_pkt_offset    = 0;
+  logpkt_tab_buf_saved[interface_id].tx_pkt_offset    = 0;
+  logpkt_tab_buf_saved[interface_id].rx_pkt_completed = 0;
+  logpkt_tab_buf_saved[interface_id].tx_pkt_completed = 0;
+  strcpy(logpkt_tab_buf_saved[interface_id].tx_error_log, "");
+
+  strcpy(logpkt_no_bk_tab[interface_id].interface_name, interface_name);
+  logpkt_no_bk_tab[interface_id].rx_pkt_count = 0;
+  logpkt_no_bk_tab[interface_id].tx_pkt_count = 0;
 
   nb_interfaces++;
 
@@ -244,17 +263,28 @@ void logpkt_close_op(void)
 /* ************************************************** */
 void logpkt_rx_byte_op(int interface_id, uint8_t val)
 {
-  LOGPKT_DBG("liblogpkt:log rx byte : 0x%02x\n", val);
-
-  if (logpkt_tab[interface_id].rx_pkt_offset == 0)
+  if (!logpkt_tab[interface_id].rx_pkt_completed)
     {
-      logpkt_tab[interface_id].rx_start_time = MACHINE_TIME_GET_NANO();
+      if (logpkt_tab[interface_id].rx_pkt_offset == 0)
+	{
+	  logpkt_tab[interface_id].rx_start_time = MACHINE_TIME_GET_NANO();
+	}
+      LOGPKT_DBG("liblogpkt:log rx byte: 0x%02x\n", val);
+      logpkt_tab[interface_id].rx_end_time = MACHINE_TIME_GET_NANO();
+      logpkt_tab[interface_id].current_rx_pkt[logpkt_tab[interface_id].rx_pkt_offset] = val;
+      logpkt_tab[interface_id].rx_pkt_offset++;
     }
-
-  logpkt_tab[interface_id].rx_end_time = MACHINE_TIME_GET_NANO();
-  logpkt_tab[interface_id].current_rx_pkt[logpkt_tab[interface_id].rx_pkt_offset] = val;
-  logpkt_tab[interface_id].rx_pkt_offset++;
-  logpkt_tab[interface_id].rx_pkt_completed = 0;
+  else
+    { /* put data in buffer as previous packet is completed but not dumped yet */
+      if (logpkt_tab_buf[interface_id].rx_pkt_offset == 0)
+	{
+	  logpkt_tab_buf[interface_id].rx_start_time = MACHINE_TIME_GET_NANO();
+	}
+      LOGPKT_DBG("liblogpkt:log rx byte in buffer: 0x%02x\n", val);
+      logpkt_tab_buf[interface_id].rx_end_time = MACHINE_TIME_GET_NANO();
+      logpkt_tab_buf[interface_id].current_rx_pkt[logpkt_tab_buf[interface_id].rx_pkt_offset] = val;
+      logpkt_tab_buf[interface_id].rx_pkt_offset++;
+    }
 }
 
 
@@ -262,15 +292,28 @@ void logpkt_tx_byte_op(int interface_id, uint8_t val)
 {
   LOGPKT_DBG("liblogpkt:log tx byte : 0x%02x\n", val);
 
-  if (logpkt_tab[interface_id].tx_pkt_offset == 0)
+  if (!logpkt_tab[interface_id].rx_pkt_completed)
     {
-      logpkt_tab[interface_id].tx_start_time = MACHINE_TIME_GET_NANO();
+      if (logpkt_tab[interface_id].tx_pkt_offset == 0)
+	{
+	  logpkt_tab[interface_id].tx_start_time = MACHINE_TIME_GET_NANO();
+	}
+      LOGPKT_DBG("liblogpkt:log tx byte : 0x%02x\n", val);
+      logpkt_tab[interface_id].tx_end_time = MACHINE_TIME_GET_NANO();
+      logpkt_tab[interface_id].current_tx_pkt[logpkt_tab[interface_id].tx_pkt_offset] = val;
+      logpkt_tab[interface_id].tx_pkt_offset++;
     }
-
-  logpkt_tab[interface_id].tx_end_time = MACHINE_TIME_GET_NANO();
-  logpkt_tab[interface_id].current_tx_pkt[logpkt_tab[interface_id].tx_pkt_offset] = val;
-  logpkt_tab[interface_id].tx_pkt_offset++;
-  logpkt_tab[interface_id].tx_pkt_completed = 0;
+  else
+    { /* put data in buffer as previous packet is completed but not dumped yet */
+      if (logpkt_tab_buf[interface_id].tx_pkt_offset == 0)
+	{
+	  logpkt_tab_buf[interface_id].tx_start_time = MACHINE_TIME_GET_NANO();
+	}
+      LOGPKT_DBG("liblogpkt:log tx byte in buffer: 0x%02x\n", val);
+      logpkt_tab_buf[interface_id].tx_end_time = MACHINE_TIME_GET_NANO();
+      logpkt_tab_buf[interface_id].current_tx_pkt[logpkt_tab_buf[interface_id].tx_pkt_offset] = val;
+      logpkt_tab_buf[interface_id].tx_pkt_offset++;
+    }
 }
 
 
@@ -278,15 +321,31 @@ void logpkt_tx_byte_op(int interface_id, uint8_t val)
 /* ************************************************** */
 void logpkt_rx_complete_pkt_op(int interface_id)
 {
-  LOGPKT_DBG("liblogpkt:rx complete pkt\n");
-  logpkt_tab[interface_id].rx_pkt_completed = 1;
+  if (!logpkt_tab[interface_id].rx_pkt_completed && logpkt_tab[interface_id].rx_pkt_offset)
+    {
+      LOGPKT_DBG("liblogpkt:rx complete pkt\n");
+      logpkt_tab[interface_id].rx_pkt_completed = 1;
+    }
+  else if (logpkt_tab_buf[interface_id].rx_pkt_offset)
+    {
+      LOGPKT_DBG("liblogpkt:rx complete pkt buf\n");
+      logpkt_tab_buf[interface_id].rx_pkt_completed = 1;
+    }
 }
 
 
 void logpkt_tx_complete_pkt_op(int interface_id)
 {
-  LOGPKT_DBG("liblogpkt:tx complete pkt\n");
-  logpkt_tab[interface_id].tx_pkt_completed = 1;
+  if (!logpkt_tab[interface_id].tx_pkt_completed && logpkt_tab[interface_id].tx_pkt_offset)
+    {
+      LOGPKT_DBG("liblogpkt:tx complete pkt\n");
+      logpkt_tab[interface_id].tx_pkt_completed = 1;
+    }
+  else if (logpkt_tab_buf[interface_id].tx_pkt_offset)
+    {
+      LOGPKT_DBG("liblogpkt:tx complete pkt buf\n");
+      logpkt_tab_buf[interface_id].tx_pkt_completed = 1;
+    }
 }
 
 
@@ -295,11 +354,24 @@ void logpkt_rx_abort_pkt_op(int interface_id, const char* error)
   char error_str[MAXERRORLENGTH + 20] = " -> rx aborted: ";
 
   LOGPKT_DBG("liblogpkt:rx abort pkt\n");
-  /* current rx frame not empty */
-  if (logpkt_tab[interface_id].rx_pkt_offset)
+
+  if (!logpkt_tab[interface_id].rx_pkt_completed && logpkt_tab[interface_id].rx_pkt_offset)
     {
-      logpkt_tab[interface_id].rx_pkt_completed = 1;
-      strcpy(logpkt_tab[interface_id].rx_error_log, strcat(error_str, error));
+      /* current rx frame not empty */
+      if (logpkt_tab[interface_id].rx_pkt_offset)
+	{
+	  logpkt_tab[interface_id].rx_pkt_completed = 1;
+	  strcpy(logpkt_tab[interface_id].rx_error_log, strcat(error_str, error));
+	}
+    }
+  else if (logpkt_tab_buf[interface_id].rx_pkt_offset)
+    {
+      /* current buffered rx frame not empty */
+      if (logpkt_tab_buf[interface_id].rx_pkt_offset)
+	{
+	  logpkt_tab_buf[interface_id].rx_pkt_completed = 1;
+	  strcpy(logpkt_tab_buf[interface_id].rx_error_log, strcat(error_str, error));
+	}
     }
 }
 
@@ -309,64 +381,77 @@ void logpkt_tx_abort_pkt_op(int interface_id, const char* error)
   char error_str[MAXERRORLENGTH + 20] = " -> tx aborted: ";
 
   LOGPKT_DBG("liblogpkt:tx abort pkt\n");
-  /* current tx frame not empty */
-  if (logpkt_tab[interface_id].tx_pkt_offset)
+
+  if (!logpkt_tab[interface_id].rx_pkt_completed && logpkt_tab[interface_id].rx_pkt_offset)
     {
-      logpkt_tab[interface_id].tx_pkt_completed = 1;
-      strcpy(logpkt_tab[interface_id].tx_error_log, strcat(error_str, error));
+      /* current tx frame not empty */
+      if (logpkt_tab[interface_id].tx_pkt_offset)
+	{
+	  logpkt_tab[interface_id].tx_pkt_completed = 1;
+	  strcpy(logpkt_tab[interface_id].tx_error_log, strcat(error_str, error));
+	}
+    }
+  else if (logpkt_tab_buf[interface_id].tx_pkt_offset)
+    {
+      /* current buffered tx frame not empty */
+      if (logpkt_tab_buf[interface_id].tx_pkt_offset)
+	{
+	  logpkt_tab_buf[interface_id].tx_pkt_completed = 1;
+	  strcpy(logpkt_tab_buf[interface_id].tx_error_log, strcat(error_str, error));
+	}
     }
 }
 
 /* ************************************************** */
 /* ************************************************** */
-static void logpkt_rx_dump_pkt(int interface_id)
+static void logpkt_rx_dump_pkt(struct _logpkt_state_t *logpkt, int interface_id)
 {
   int i;
 
   /* current rx frame not empty */
-  if (logpkt_tab[interface_id].rx_pkt_offset && log_mode != LOG_TX_ONLY)
+  if (logpkt->rx_pkt_offset && log_mode != LOG_TX_ONLY)
     {
       fprintf(logpkt_logfile,"\n");
-      fprintf(logpkt_logfile,"******* %s: RX packet (nÂ°%d) [%"PRIu64"ns; %"PRIu64"ns] *******\n",
-	      logpkt_tab[interface_id].interface_name,
-	      logpkt_tab[interface_id].rx_pkt_count,
-	      logpkt_tab[interface_id].rx_start_time,
-	      logpkt_tab[interface_id].rx_end_time);
+      fprintf(logpkt_logfile,"******* %s: RX packet (n°%d) [%"PRIu64"ns; %"PRIu64"ns] *******\n",
+	      logpkt_no_bk_tab[interface_id].interface_name,
+	      logpkt_no_bk_tab[interface_id].rx_pkt_count,
+	      logpkt->rx_start_time,
+	      logpkt->rx_end_time);
 
-      for (i = 0; i < logpkt_tab[interface_id].rx_pkt_offset - 1; i++)
+      for (i = 0; i < logpkt->rx_pkt_offset - 1; i++)
 	{
-	  fprintf(logpkt_logfile,"%02x:", logpkt_tab[interface_id].current_rx_pkt[i]);
+	  fprintf(logpkt_logfile,"%02x:", logpkt->current_rx_pkt[i]);
 	}
-      fprintf(logpkt_logfile,"%02x", logpkt_tab[interface_id].current_rx_pkt[logpkt_tab[interface_id].rx_pkt_offset - 1]);
-      fprintf(logpkt_logfile,"%s\n", logpkt_tab[interface_id].rx_error_log);
+      fprintf(logpkt_logfile,"%02x", logpkt->current_rx_pkt[logpkt->rx_pkt_offset - 1]);
+      fprintf(logpkt_logfile,"%s\n", logpkt->rx_error_log);
 
-      logpkt_tab[interface_id].rx_pkt_count++;
+      logpkt_no_bk_tab[interface_id].rx_pkt_count++;
     }
 }
 
 
-static void logpkt_tx_dump_pkt(int interface_id)
+static void logpkt_tx_dump_pkt(struct _logpkt_state_t *logpkt, int interface_id)
 {
   int i;
 
   /* current tx frame not empty */
-  if (logpkt_tab[interface_id].tx_pkt_offset && log_mode != LOG_RX_ONLY)
+  if (logpkt->tx_pkt_offset && log_mode != LOG_RX_ONLY)
     {
       fprintf(logpkt_logfile,"\n");
-      fprintf(logpkt_logfile,"******* %s: TX packet (nÂ°%d) [%"PRIu64"ns; %"PRIu64"ns] *******\n",
-	      logpkt_tab[interface_id].interface_name,
-	      logpkt_tab[interface_id].tx_pkt_count,
-	      logpkt_tab[interface_id].tx_start_time,
-	      logpkt_tab[interface_id].tx_end_time);
+      fprintf(logpkt_logfile,"******* %s: TX packet (n°%d) [%"PRIu64"ns; %"PRIu64"ns] *******\n",
+	      logpkt_no_bk_tab[interface_id].interface_name,
+	      logpkt_no_bk_tab[interface_id].tx_pkt_count,
+	      logpkt->tx_start_time,
+	      logpkt->tx_end_time);
 
-      for (i = 0; i < logpkt_tab[interface_id].tx_pkt_offset - 1; i++)
+      for (i = 0; i < logpkt->tx_pkt_offset - 1; i++)
 	{
-	  fprintf(logpkt_logfile,"%02x:", logpkt_tab[interface_id].current_tx_pkt[i]);
+	  fprintf(logpkt_logfile,"%02x:", logpkt->current_tx_pkt[i]);
 	}
-      fprintf(logpkt_logfile,"%02x", logpkt_tab[interface_id].current_tx_pkt[logpkt_tab[interface_id].tx_pkt_offset - 1]);
-      fprintf(logpkt_logfile,"%s\n", logpkt_tab[interface_id].tx_error_log);
+      fprintf(logpkt_logfile,"%02x", logpkt->current_tx_pkt[logpkt->tx_pkt_offset - 1]);
+      fprintf(logpkt_logfile,"%s\n", logpkt->tx_error_log);
 
-      logpkt_tab[interface_id].tx_pkt_count++;
+      logpkt_no_bk_tab[interface_id].tx_pkt_count++;
     }
 }
 
@@ -374,33 +459,84 @@ static void logpkt_tx_dump_pkt(int interface_id)
 /* ************************************************** */
 void logpkt_state_save_op(void)
 {
-  int i;
+  int i,j;
   
   for (i = 0; i < nb_interfaces; i++)
-    {
+    { 
+      /* rx packet */
       if (logpkt_tab[i].rx_pkt_completed)
 	{
-	  logpkt_rx_dump_pkt(i);
+	  LOGPKT_DBG("liblogpkt: dump tx pkt\n");
+	  logpkt_rx_dump_pkt(&logpkt_tab[i], i);
 	  logpkt_tab[i].rx_pkt_completed = 0;
 	  logpkt_tab[i].rx_pkt_offset    = 0;
 	  strcpy(logpkt_tab[i].rx_error_log, "");
+	  
+	  if (logpkt_tab_buf[i].rx_pkt_offset)
+	    { /* data is buffered */
+	      if (logpkt_tab_buf[i].rx_pkt_completed)
+		{ /* new frame is completed */
+		  LOGPKT_DBG("liblogpkt: dump tx pkt buf\n");
+		  logpkt_rx_dump_pkt(&logpkt_tab_buf[i], i);
+		}
+	      else
+		{ /* new frame ongoing: move it to logpkt_tab */
+		  LOGPKT_DBG("liblogpkt: copy tx pkt buf to current tx pkt\n");
+		  memcpy(logpkt_tab[i].current_rx_pkt, logpkt_tab_buf[i].current_rx_pkt,
+			 sizeof(uint8_t) * logpkt_tab_buf[i].rx_pkt_offset);
+		  logpkt_tab[i].rx_pkt_offset = logpkt_tab_buf[i].rx_pkt_offset;
+		  logpkt_tab[i].rx_start_time = logpkt_tab_buf[i].rx_start_time;
+		  logpkt_tab[i].rx_end_time   = logpkt_tab_buf[i].rx_end_time;
+		}
+	      /* delete buffered data */
+	      logpkt_tab_buf[i].rx_pkt_completed = 0;
+	      logpkt_tab_buf[i].rx_pkt_offset    = 0;
+	      strcpy(logpkt_tab_buf[i].rx_error_log, "");
+	    }
 	}
+
+      /* tx packet */
       if (logpkt_tab[i].tx_pkt_completed)
 	{
-	  logpkt_tx_dump_pkt(i);
+	  LOGPKT_DBG("liblogpkt: dump rx pkt\n");
+	  logpkt_tx_dump_pkt(&logpkt_tab[i], i);
 	  logpkt_tab[i].tx_pkt_completed = 0;
 	  logpkt_tab[i].tx_pkt_offset    = 0;
 	  strcpy(logpkt_tab[i].tx_error_log, "");
+
+	  if (logpkt_tab_buf[i].tx_pkt_offset)
+	    { /* data is buffered */
+	      if (logpkt_tab_buf[i].tx_pkt_completed)
+		{ /* new frame is completed */
+		  LOGPKT_DBG("liblogpkt: dump rx pkt buf\n");
+		  logpkt_tx_dump_pkt(&logpkt_tab_buf[i], i);
+		}
+	      else
+		{ /* new frame ongoing: move it to logpkt_tab */
+		  LOGPKT_DBG("liblogpkt: copy rx pkt buf to current rx pkt\n");
+		  memcpy(logpkt_tab[i].current_tx_pkt, logpkt_tab_buf[i].current_tx_pkt,
+			 sizeof(uint8_t) * logpkt_tab_buf[i].tx_pkt_offset);
+		  logpkt_tab[i].tx_pkt_offset = logpkt_tab_buf[i].tx_pkt_offset;
+		  logpkt_tab[i].tx_start_time = logpkt_tab_buf[i].tx_start_time;
+		  logpkt_tab[i].tx_end_time   = logpkt_tab_buf[i].tx_end_time;
+		}
+	      /* delete buffered data */
+	      logpkt_tab_buf[i].tx_pkt_completed = 0;
+	      logpkt_tab_buf[i].tx_pkt_offset    = 0;
+	      strcpy(logpkt_tab_buf[i].tx_error_log, "");
+	    }
 	}
     }
 
   memcpy(logpkt_tab_saved, logpkt_tab, nb_interfaces * sizeof(struct _logpkt_state_t));
+  memcpy(logpkt_tab_buf_saved, logpkt_tab_buf, nb_interfaces * sizeof(struct _logpkt_state_t));
 }
 
 
 void logpkt_state_restore_op(void)
 {
   memcpy(logpkt_tab, logpkt_tab_saved, nb_interfaces * sizeof(struct _logpkt_state_t));
+  memcpy(logpkt_tab_buf, logpkt_tab_buf_saved, nb_interfaces * sizeof(struct _logpkt_state_t));
 }
 
 

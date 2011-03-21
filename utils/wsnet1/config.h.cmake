4118aa879f02554f146f873a616d9d8979ae95be
afraboul <afraboul@c0ef0dea-404d-0410-ad65-84cceb5f200a>
2011-03-21 22:52:39 +0000

    start integrating CMake in WSim :: wtracer and wsnet1
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/wsim@567 c0ef0dea-404d-0410-ad65-84cceb5f200a

---
+++ utils/wsnet1/config.h.cmake
@@ -0,0 +1,16 @@
+#define PACKAGE_STRING  "wsnet1"
+#define PACKAGE_VERSION "1.0"
+
+#cmakedefine HAVE_STRINGS_H 1
+#cmakedefine HAVE_INTTYPES_H 1
+#cmakedefine HAVE_STDINT_H 1
+#cmakedefine HAVE_SYS_MOUNT_H
+
+#cmakedefine FUNC_STRTOK_R_DEFINED 1
+
+#cmakedefine SIZEOF_CHAR      @SIZEOF_CHAR@
+#cmakedefine SIZEOF_SHORT     @SIZEOF_SHORT@
+#cmakedefine SIZEOF_INT       @SIZEOF_INT@
+#cmakedefine SIZEOF_LONG      @SIZEOF_LONG@
+#cmakedefine SIZEOF_LONG_LONG @SIZEOF_LONG_LONG@
+

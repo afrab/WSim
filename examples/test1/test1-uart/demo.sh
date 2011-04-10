a65d3913fc3ff912d34ff993ea7661f9e386a11e
afraboul <afraboul@c0ef0dea-404d-0410-ad65-84cceb5f200a>
2011-04-10 08:59:48 +0000

    - update demo scripts
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/wsim@577 c0ef0dea-404d-0410-ad65-84cceb5f200a

--- examples/test1/test1-uart/demo.sh
+++ examples/test1/test1-uart/demo.sh
@@ -1,26 +1,54 @@
 #! /bin/sh
 
-rm -f trace*.eps trace*.gp trace*.jpg
+# Serial terminal emulation
+#  stdout | UDP | TCP
+SERMODE="UDP"
 
-CALLGRINDOPTS="--dump-line=yes --dump-instr=yes --trace-jump=yes --collect-systime=yes --collect-jumps=yes --combine-dumps=yes"
-DEBUG="callgrind ${CALLGRINDOPTS}"
-DEBUG=""
-
-${DEBUG} ./wsim --ui --serial1=$1 --trace=trace --tracemode=gplot  uart.elf 
-
-GP=`echo *.gp`
-if [ "$GP" != "*.gp" ] ; then 
-    for i in ${GP} ; do 
-	echo "============= $i"
-        gnuplot < $i 
-    done
+if [ "x`which nc.traditional`" = "x" ]
+then
+    NETCAT=nc
+else
+    NETCAT=nc.traditional
 fi
 
-EPS=`echo *.eps`
-if [ "$EPS" != "*.eps" ] ; then 
-    for i in ${EPS} ; do 
-	gv $i & 
-    done
+case $SERMODE in
+    "stdout")
+        SERIAL="--serial1_io=stdout"
+        NCCMD=""
+        ;;
+    "UDP")
+        NCCMD="${NETCAT} -u -p 7000 localhost 6000"
+        SERIAL="--serial1_io=udp:localhost:6000:localhost:7000"
+        ;;
+    "TCP")
+        NCCMD="${NETCAT} localhost 6000"
+        SERIAL="--serial1_io=bk:tcp:s:localhost:6000"
+        ;;
+esac
+
+## ##################################################
+## ##################################################
+
+WSIM=wsim-msp1611
+WSIM=~/tmp/wsim/platforms/tests/wsim-msp1611
+
+${WSIM}  --ui ${SERIAL} --trace=wsim.trc uart.elf &
+
+## ##################################################
+## ##################################################
+
+if [ "${NCCMD}" != "" ] ; then
+    xterm -T netcat-1 -e "${NCCMD}" &
+    echo "${NCCMD}"
 fi
+
+## ##################################################
+## ##################################################
+
+read dummyval
+killall -SIGUSR1 ${WSIM}   > /dev/null 2>&1
+killall -SIGQUIT ${NETCAT} > /dev/null 2>&1
+
+wtracer --in=wsim.trc --out=wsim.vcd --format=vcd
 
 

6764a8005e9064ec4932d60c3a2f847b06c13335
jcarpentier <jcarpentier@c0ef0dea-404d-0410-ad65-84cceb5f200a>
2011-06-17 16:00:33 +0000

    add ez430-rf2500 examples
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/wsim@623 c0ef0dea-404d-0410-ad65-84cceb5f200a

---
+++ examples/ez430rf/read/.deps/main.d
@@ -0,0 +1,13 @@
+.obj/main.o: src/main.c ../drivers/inc/leds.h ../drivers/inc/clock.h \
+ ../drivers/inc/watchdog.h ../drivers/inc/uart.h \
+ ../drivers/inc/lpm_compat.h
+
+../drivers/inc/leds.h:
+
+../drivers/inc/clock.h:
+
+../drivers/inc/watchdog.h:
+
+../drivers/inc/uart.h:
+
+../drivers/inc/lpm_compat.h:

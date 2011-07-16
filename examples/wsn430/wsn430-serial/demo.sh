d2afdd385995ac4bdc0862281d496cab0d596451
Antoine Fraboulet <antoine.fraboulet@insa-lyon.fr>
2011-07-16 22:47:19 +0200

    update demo script

--- examples/wsn430/wsn430-serial/demo.sh
+++ examples/wsn430/wsn430-serial/demo.sh
@@ -12,33 +12,10 @@ LOGFILE=wsim.log
 TRACEFILE=wsim.trc
 GUI=yes
 
-MODE=time
+MODE=run
 TIME=60s
 
-# Serial 0
-SERIAL[0]=""
-SERIAL_TERM[0]=""
-
-# Serial 1
-SERMODE=TCP
-case "$SERMODE" in
-    "stdout")
-	SERIAL[1]="stdout"
-	SERIAL_TERM[1]=""
-	;;
-    "UDP")
-	SERIAL[1]="udp:localhost:6000:localhost:7000"
-	SERIAL_TERM[1]="${NETCAT} -u localhost 6000"
-	;;
-    "TCP")
-	SERIAL[1]="tcp:s:localhost:6000"
-	SERIAL_TERM[1]="${NETCAT} localhost 6000"
-	;;
-    *)
-	SERIAL[1]=""
-	SERIAL_TERM[1]=""
-	;;
-esac
+SERIAL[1]="tcp:netcat"
 
 ## ============= Run =====================
 

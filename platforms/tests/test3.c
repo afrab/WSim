7650bf875b7a7f8b895486709c32edd6e451af14
afraboul <afraboul@c0ef0dea-404d-0410-ad65-84cceb5f200a>
2011-11-16 19:26:09 +0000

    update gui init
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/wsim@722 c0ef0dea-404d-0410-ad65-84cceb5f200a

--- platforms/tests/test3.c
+++ platforms/tests/test3.c
@@ -191,6 +191,7 @@ int devices_reset_post()
   SPIDEV_CSn = 0;
   SPIDEV_Wn  = 0;
 
+  REFRESH(LOGO1);
   REFRESH(LED1);
   REFRESH(LED2);
   REFRESH(LED3);
@@ -199,7 +200,6 @@ int devices_reset_post()
   REFRESH(LED6);
   REFRESH(LED7);
   REFRESH(LED8);
-  REFRESH(LOGO1);
   ui_refresh(refresh);
   return 0;
 }
